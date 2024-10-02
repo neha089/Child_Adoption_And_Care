66aff596f5ecceae1f9f02840448b46b3960de96
neha089 <nehapatel30325@gmail.com>
2024-10-02 09:45:09 +0530

    sql file

--- child_a_c/SQLQuery1.sql
+++ child_a_c/SQLQuery1.sql
@@ -1,14 +1,4 @@
-﻿
-CREATE TABLE Adopters (
-    adopter_id INT PRIMARY KEY IDENTITY(1,1),
-    first_name NVARCHAR(100) NOT NULL,
-    last_name NVARCHAR(100) NOT NULL,
-    password NVARCHAR(100) NOT NULL,
-    date_of_birth DATE NOT NULL,
-    address NVARCHAR(255) NOT NULL,
-    phone_number NVARCHAR(20) NOT NULL,
-    email NVARCHAR(100) NOT NULL,
-    marital_status NVARCHAR(50) NOT NULL,
-    occupation NVARCHAR(100) NULL,
-    education_level NVARCHAR(100) NULL
-);
+﻿SELECT orphanage_id 
+FROM Orphanages 
+WHERE email = 'qwer@gmail.com' 
+AND password = '0987';
